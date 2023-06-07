d0f0f96dfc9bd985fb6025b755896722ef50896e
OUSSAMA SEFIANI <oussama.sefiani@um5r.ac.ma>
2023-06-07 22:09:57 +0000

    test

---
+++ 0x00-python-hello_world/10-linked_lists.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint - printsst
+ * @h: pointer
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+    const listint_t *current;
+    unsigned int n; /* number of nodes */
+
+    current = h;
+    n = 0;
+    while (current != NULL)
+    {
+        printf("%i\n", current->n);
+        current = current->next;
+        n++;
+    }
+
+    return (n);
+}
+
+/**
+ * add_nodeint - adds a new node beginning of a  list
+ * @head: points to the first
+ * @n: int to an ode
+ * Return: address yhs
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+    listint_t *new;
+
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+        return (NULL);
+
+    new->n = n;
+    new->next = *head;
+    *head = new;
+
+    return (new);
+}
+
+/**
+ * free_listint - freest
+ * @head: points to free the list
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+    listint_t *current;
+
+    while (head != NULL)
+    {
+        current = head;
+        head = head->next;
+        free(current);
+    }
+}
