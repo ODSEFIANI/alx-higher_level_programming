b002a676565c5b034ccd2d26dee34232e8f304d1
OUSSAMA SEFIANI <oussama.sefiani@um5r.ac.ma>
2023-06-28 00:01:22 +0000

    python clases

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""class Square defines a square by size"""
+
+
+class Square:
+    """ class Square that defines a square"""
+    def __init__(self, size):
+        """initialize square
+        Args:
+            size (int): size of square
+        """
+        self.__size = size  #: size of  square
