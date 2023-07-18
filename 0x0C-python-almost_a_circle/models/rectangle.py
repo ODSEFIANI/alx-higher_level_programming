624879fa2bbd4f8998fbf1c377168d202f175ea8
ODSEFIANI <92064461+ODSEFIANI@users.noreply.github.com>
2023-07-18 16:15:19 +0100

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -105,47 +105,49 @@
                 [print(" ", end="") for x in range(self.x)]
                 [print("#", end="") for w in range(self.width)]
                 print("")
+                
         def update(self, *args, **kwargs):
-        """set and Update the Rectangle class.
-
-        Args:
-            *args (ints): Nees.
-                - 1st argumattribute
-            **kwargs (dict): New f attributes.
-        """
-        if args and len(args) != 0:
-            a = 0
-            for arg in args:
-                if a == 0:
-                    if arg is None:
-                        self.__init__(self.width, self.height, self.x, self.y)
-                    else:
-                        self.id = arg
-                elif a == 1:
-                    self.width = arg
-                elif a == 2:
-                    self.height = arg
-                elif a == 3:
-                    self.x = arg
-                elif a == 4:
-                    self.y = arg
-                a += 1
-
-        elif kwargs and len(kwargs) != 0:
-            for key, value in kwargs.items():
-                if key == "id":
-                    if value is None:
-                        self.__init__(self.width, self.height, self.x, self.y)
-                    else:
-                        self.id = value
-                elif key == "width":
-                    self.width = value
-                elif key == "height":
-                    self.height = value
-                elif key == "x":
-                    self.x = value
-                elif key == "y":
-                    self.y = value
+            
+            """set and Update the Rectangle class.
+    
+            Args:
+                *args (ints): Nees.
+                    - 1st argumattribute
+                **kwargs (dict): New f attributes.
+            """
+            if args and len(args) != 0:
+                a = 0
+                for arg in args:
+                    if a == 0:
+                        if arg is None:
+                            self.__init__(self.width, self.height, self.x, self.y)
+                        else:
+                            self.id = arg
+                    elif a == 1:
+                        self.width = arg
+                    elif a == 2:
+                        self.height = arg
+                    elif a == 3:
+                        self.x = arg
+                    elif a == 4:
+                        self.y = arg
+                    a += 1
+    
+            elif kwargs and len(kwargs) != 0:
+                for key, value in kwargs.items():
+                    if key == "id":
+                        if value is None:
+                            self.__init__(self.width, self.height, self.x, self.y)
+                        else:
+                            self.id = value
+                    elif key == "width":
+                        self.width = value
+                    elif key == "height":
+                        self.height = value
+                    elif key == "x":
+                        self.x = value
+                    elif key == "y":
+                        self.y = value
 
         def to_dictionary(self):
         """
